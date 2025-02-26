5c766d3d7056345ae7b01ec666937d304978c4d7
Amran <124218@swcsd.us>
2025-02-26 12:48:26 -0500

    Initial Commit

---
+++ Animation2/Animation2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation2App.swift
+//  Animation2
+//
+//  Created by Amran Abdullahi on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Animation2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation2Tests/Animation2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Animation2Tests.swift
+//  Animation2Tests
+//
+//  Created by Amran Abdullahi on 2/26/25.
+//
+
+import Testing
+@testable import Animation2
+
+struct Animation2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
