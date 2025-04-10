2952ce209b3eedc4e4972a041686d3e8d1bd1956
Amran <124218@swcsd.us>
2025-04-10 15:01:30 -0400

    Worked on categories

---
+++ scrap/scrap/PuzzleDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PuzzleDetail.swift
+//  scrap
+//
+//  Created by Amran Abdullahi on 4/10/25.
+//
+
+import SwiftUI
+
+struct PuzzleDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PuzzleDetail()
+}
