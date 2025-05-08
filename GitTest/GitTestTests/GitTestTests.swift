b0f332a6ed6869f55908fb69d328ab7e0162d836
Amran <124218@swcsd.us>
2025-05-08 12:52:42 -0400

    Test

---
+++ GitTest/GitTestTests/GitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestTests.swift
+//  GitTestTests
+//
+//  Created by Amran Abdullahi on 5/8/25.
+//
+
+import Testing
+@testable import GitTest
+
+struct GitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
